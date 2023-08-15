7f30c04580ef164b830a590792e7a0cee86b6661
Jim Kang <jimkang@gmail.com>
2023-08-14 22:13:42 -0400

    Load source audio.

--- main.ts
+++ main.ts
@@ -5,10 +5,17 @@ import { version } from './package.json';
 import seedrandom from 'seedrandom';
 import RandomId from '@jimkang/randomid';
 import { createProbable as Probable } from 'probable';
+import OLPE from 'one-listener-per-element';
+import { getAudioBufferFromFile } from 'synthskel/tasks/get-audio-buffer-from-file';
+import { renderResultAudio } from './renderers/render-result-audio';
 
 var randomId = RandomId();
+var { on } = OLPE();
 var prob: any;
 var urlStore: any;
+var fileInput: HTMLInputElement = document.getElementById(
+  'file'
+) as HTMLInputElement;
 
 (async function go() {
   window.onerror = reportTopLevelError;
@@ -33,6 +40,47 @@ async function onUpdate(
   var random = seedrandom(state.seed);
   prob = Probable({ random });
   prob.roll(2);
+
+  wireControls(
+    Object.assign(
+      { onFileChange, onSegmentChange, onScrambleChange, onScramble },
+      state
+    )
+  );
+}
+
+function wireControls({
+  onFileChange,
+  onSegmentChange,
+  onScrambleChange,
+  onScramble,
+  segmentCount,
+  scrambleRunCount,
+}) {
+  on('#file', 'change', onFileChange);
+}
+
+function onSegmentChange() {}
+function onScrambleChange() {}
+function onScramble() {}
+
+async function onFileChange() {
+  if (!fileInput?.files?.length) {
+    return;
+  }
+
+  try {
+    var audioBuffer = await getAudioBufferFromFile({
+      file: fileInput.files[0],
+    });
+
+    renderResultAudio({
+      audioBuffer,
+      containerSelector: '.source-audio-container',
+    });
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 function reportTopLevelError(
